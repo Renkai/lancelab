0ee5acef97b40463264182cf94b87882b55e67d4
Renkai Ge <renkaige@tubi.tv>
2023-02-20 22:09:16 +0800

    System.loadLibrary

--- src/main/java/cinterface/Converter.java
+++ src/main/java/cinterface/Converter.java
@@ -9,11 +9,14 @@ import org.apache.arrow.vector.BigIntVector;
 import org.apache.arrow.vector.FieldVector;
 
 public class Converter {
-    native public static Long[] getInt32Arr();
+
+    static {
+        System.loadLibrary("lancelab");
+    }
 
     public static void getInt32ArrayExample() {
         BufferAllocator allocator = new RootAllocator();
-        Long[] arr = getInt32Arr();
+        Long[] arr = ConverterJni.getInt32Arr();
         try (ArrowSchema arrowSchema = ArrowSchema.wrap(arr[0]); ArrowArray array = ArrowArray.wrap(arr[1])) {
             var vec = Data.importVector(allocator, array, arrowSchema, null);
             System.out.println("rust allocated array: " + vec);
---
+++ src/main/java/cinterface/ConverterJni.java
@@ -0,0 +1,5 @@
+package cinterface;
+
+public class ConverterJni {
+    native public static Long[] getInt32Arr();
+}
